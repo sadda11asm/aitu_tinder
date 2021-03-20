8c1a57cd187c1b2896401f66fa929da9223cec3b
Saddam <saddam.asmatullayev@nu.edu.kz>
2021-03-21 04:39:19 +0600

    Fix algo for search

--- app/controllers/hacknu_users_controller.rb
+++ app/controllers/hacknu_users_controller.rb
@@ -115,10 +115,11 @@ class HacknuUsersController < ApplicationController
   R = 6371.0
 
   def square(lng, lat, rad)
-    maxLat = lat + ((rad.to_f / R) * Math::PI / 180)
-    minLat = lat - ((rad.to_f / R) * Math::PI / 180)
-    maxLng = lng + (Math.asin(rad.to_f / R) / Math.cos(lat / 180 * Math::PI)) * Math::PI / 180
-    minLng = lng - (Math.asin(rad.to_f / R) / Math.cos(lat / 180 * Math::PI)) * Math::PI / 180
+    rad /= 1000
+    maxLat = lat + ((rad.to_f / R) * Math::PI / 180)*1000
+    minLat = lat - ((rad.to_f / R) * Math::PI / 180)*1000
+    maxLng = lng + (Math.asin(rad.to_f / R) / Math.cos(lat / 180 * Math::PI)) * Math::PI / 180*1000
+    minLng = lng - (Math.asin(rad.to_f / R) / Math.cos(lat / 180 * Math::PI)) * Math::PI / 180*1000
     [minLng, maxLng, minLat, maxLat]
   end
 
